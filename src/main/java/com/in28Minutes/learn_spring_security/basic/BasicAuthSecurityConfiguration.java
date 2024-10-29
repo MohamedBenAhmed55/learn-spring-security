f6e59e17a1a41eff305a8e587f9adf0d7550e43b
MohamedBenAhmed55 <mohamed.benahmed@etudiant-fsegt.utm.tn>
2024-10-29 23:08:05 +0100

    Save users to h2base and encode password

--- src/main/java/com/in28Minutes/learn_spring_security/basic/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/in28Minutes/learn_spring_security/basic/BasicAuthSecurityConfiguration.java
@@ -2,9 +2,19 @@ package com.in28Minutes.learn_spring_security.basic;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -25,9 +35,67 @@ public class BasicAuthSecurityConfiguration {
 
         http.httpBasic(withDefaults());
 
+        // Disables CSRF
         http.csrf(csrf -> csrf.disable());
 
+        // Enables frames for h2base console
+        http.headers(headers -> headers.frameOptions(frameOptionsConfig -> frameOptionsConfig.disable()));
+
         return http.build();
+    }
+
+    // Creating users only For test purposes
+    /*@Bean
+    public UserDetailsService userDetailsService(){
+        var user = User.withUsername("in28minutes")
+                .password("{noop}dummy")
+                .roles("USER")
+                .build();
+
+        var admin = User.withUsername("admin")
+                .password("{noop}dummy")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user,admin);
+    }*/
+
+    // Creating own Data Source for Users
+    @Bean
+    public DataSource dataSource(){
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
+    }
+
+    // Adding Users with jdbc default schema location
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource){
+        var user = User.withUsername("in28minutes")
+//                .password("{noop}dummy")
+                .password("dummy")
+                .passwordEncoder(str -> passwordEncoder().encode(str))
+                .roles("USER")
+                .build();
+
+        var admin = User.withUsername("admin")
+//                .password("{noop}dummy")
+                .password("dummy")
+                .passwordEncoder(str -> passwordEncoder().encode(str))
+                .roles("ADMIN, USER")
+                .build();
+
+        var jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.createUser(user);
+        jdbcUserDetailsManager.createUser(admin);
+
+        return jdbcUserDetailsManager;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
